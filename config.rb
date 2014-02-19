43c204534b3a16c841d6f32554733fabcaca15aa
Kenzie Rudolph-Wilson <kenzie@prospr.ca>
2014-02-19 16:35:28 -0500

    first commit

---
+++ config.rb
@@ -0,0 +1,26 @@
+# Require any additional compass plugins here.
+require "susy"
+
+# Settings
+css_dir = "assets"        
+sass_dir = "sass"
+images_dir = "assets"
+javascripts_dir = "js"
+fonts_dir = "fonts"
+
+output_style = :nested
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+line_comments = true
+color_output = false
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run from within the 'prospr' folder (and no deeper):
+# sass-convert -R --from scss --to sass sass scss && rm -rf sass && mv scss sass
+# sass lovers must then remember to comment out the line below:
+preferred_syntax = :scss
---
+++ gruntfile.js
@@ -0,0 +1,63 @@
+module.exports = function(grunt){
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-imagemin');
+	grunt.loadNpmTasks('grunt-shopify');
+
+	grunt.initConfig({
+		uglify: {
+			myTarget: {
+				files: {
+					'js/prospr.js': ['js/components/*.js']
+				}//files
+			}//myTarget
+		},//uglify
+		compass: {
+			dev: {
+				options: {
+					config: 'config.rb'
+				}//options
+			}//dev
+		},//compass
+		imagemin: {
+			dynamic: {
+		        files: [{
+		            expand: true,
+		            cwd: 'assets/',
+		            src: ['src/*.{png,jpg,gif}'],
+		            dest: 'assets/'
+		        }]//files
+		    }//dynamic
+		},//imagemin
+		shopify: {
+	    options: {
+	      api_key: 'b3b1c57c536f231d6d85882c16a1ad1d',
+	      password: 'ObviouslyNotThePassword',
+	      url: 'chez-noir.myshopify.com',
+	      base: '',
+	      theme: '6602185'
+	    }//options
+	  },//shopify
+		watch: {
+			scripts: {
+				files: ['js/*.js'],
+				tasks: ['uglify']
+			},//scripts
+			
+			sass: {
+				files: ['sass/*.scss'],
+				tasks: ['compass:dev']
+			},//sass
+			images: {
+				files: ['assets/src/*.{png,jpg,gif}'],
+				tasks: ['imagemin']
+			},//images
+			shopify: {
+				files: ['assets/**', 'layout/**', 'snippets/**', 'templates/**', 'config/**'],
+				tasks: ['shopify']
+			}//shopify
+		}//watch
+	})//initConfig
+	grunt.registerTask('default', 'watch');
+}//exports
